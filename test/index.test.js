b962d45dccb1f8bbf6f41176908fa3c2579911d0
Thotino GOBIN-GANSOU <gthomario@yahoo.com>
2020-07-19 08:47:11 +0200

    fix: lint fix

--- test/index.test.js
+++ test/index.test.js
@@ -2,4 +2,4 @@ const helpers = require("../index");
 
 // helpers.helpers.addObjectToIndex({foo: "bar"}).then((returnedData) => {  console.log(returnedData); });
 
-helpers.helpers.bulkIndex("type-test", [{foo: "bar"}, {foo: "baz"},]).then((returnedData) => {  console.log(returnedData); });
+helpers.helpers.bulkIndex("type-test", [{foo: "bar"}, {foo: "baz"}]).then((returnedData) => { console.log(returnedData); });
